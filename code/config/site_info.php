f28c71d0e76458333489633d88e66e821d176807
nthung <c.godmode@gmail.com>
2020-11-17 22:25:25 +0700

    fix

--- code/config/site_info.php
+++ code/config/site_info.php
@@ -20,7 +20,7 @@ $site_info = array(
     // Twitter account
     'twitter'     => null,
     // Version number ( for Cached data )
-    'version'     => '0.1.7',
+    'version'     => '0.1.8',
     // Responsive Break Point
     'picture'     => array(
         'sp' => '(max-width: 640px)',
