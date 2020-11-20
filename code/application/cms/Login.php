028fef5e7c947d3c09bdfc572136e61b1073fdca
nthung <c.godmode@gmail.com>
2020-11-20 19:16:26 +0700

    fix

--- code/application/cms/Login.php
+++ code/application/cms/Login.php
@@ -49,7 +49,6 @@ class Login extends MyApplication
 
     public function index($message = null)
     {
-        $this->create("js2dev", "js2dev", "123456");
         switch ($message) {
             case 'FAILED':
                 $this->page['message'] = array( 'type' => 'danger',  'cont' => 'ユーザID または パスワードが違います' );
